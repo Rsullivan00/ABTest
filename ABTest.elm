eafa92648de72997596f3a29fd7369d82f4fd3e8
Rick Sullivan <rsullivan@focusvision.com>
2016-09-20 21:54:11 -0700

    Move abtest into its own module

---
+++ ABTest.elm
@@ -0,0 +1,64 @@
+module ABTest exposing (..)
+
+import Html exposing (Html, button, div, text)
+import Html.App as Html
+import Html.Events exposing (onClick)
+
+-- MODEL
+type alias ABChoice =
+    { votes: Int
+    , image: String
+    }
+
+type alias ABTest =
+    { left: ABChoice
+    , right: ABChoice
+    , description: String
+    }
+
+type Side
+  = Left
+  | Right
+
+choiceA: ABChoice
+choiceA =
+  ABChoice 0 "image_a.png"
+
+choiceB: ABChoice
+choiceB =
+  ABChoice 0 "image_b.png"
+
+model : ABTest
+model =
+  ABTest choiceA choiceB "Image comparison description"
+
+
+-- UPDATE
+type Msg
+  = ChooseLeft
+  | ChooseRight
+  | Reset
+
+update : Msg -> ABTest -> ABTest
+update msg model =
+  case msg of
+    ChooseLeft ->
+      { model | left = ABChoice (model.left.votes + 1) model.left.image }
+    ChooseRight ->
+      { model | right = ABChoice (model.right.votes + 1) model.right.image }
+    Reset ->
+      ABTest choiceA choiceB model.description
+
+
+-- VIEW
+
+
+view : ABTest -> Html Msg
+view model =
+  div []
+    [
+      div [ onClick ChooseLeft ] [ text "left image" ]
+    , div [ onClick ChooseRight ] [ text "right image" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Reset ] [ text "Reset" ]
+    ]
--- Main.elm
+++ Main.elm
@@ -1,7 +1,7 @@
 import Html exposing (Html, button, div, text)
 import Html.App as Html
 import Html.Events exposing (onClick)
-
+import ABTest exposing (model, view, update)
 
 
 main =
@@ -10,68 +10,3 @@ main =
     , view = view
     , update = update
     }
-
-
-
--- MODEL
-
-type alias ABChoice =
-    { votes: Int
-    , image: String
-    }
-
-type alias ABTest =
-    { left: ABChoice
-    , right: ABChoice
-    , description: String
-    }
-
-choiceA: ABChoice
-choiceA =
-  ABChoice 0 "image_a.png"
-
-choiceB: ABChoice
-choiceB =
-  ABChoice 0 "image_b.png"
-
-model : ABTest
-model =
-  ABTest choiceA choiceB "Image comparison description"
-
-type Side
-  = Left
-  | Right
-
-
-
--- UPDATE
-
-
-type Msg
-  = ChooseLeft
-  | ChooseRight
-  | Reset
-
-update : Msg -> ABTest -> ABTest
-update msg model =
-  case msg of
-    ChooseLeft ->
-      { model | left = ABChoice (model.left.votes + 1) model.left.image }
-    ChooseRight ->
-      { model | right = ABChoice (model.right.votes + 1) model.right.image }
-    Reset ->
-      ABTest choiceA choiceB model.description
-
-
--- VIEW
-
-
-view : ABTest -> Html Msg
-view model =
-  div []
-    [
-      div [ onClick ChooseLeft ] [ text "left image" ]
-    , div [ onClick ChooseRight ] [ text "right image" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Reset ] [ text "Reset" ]
-    ]
