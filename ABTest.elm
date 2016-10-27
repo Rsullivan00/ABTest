6fd4a72bd893df463d770af3691db0e3996fc7a5
Rick Sullivan <rsullivan@focusvision.com>
2016-10-26 20:26:09 -0700

    Reformat with elm-format

--- ABTest.elm
+++ ABTest.elm
@@ -3,29 +3,40 @@ module ABTest exposing (..)
 import Html exposing (Html, button, div, text)
 import Html.App
 import Html.Events exposing (onClick)
-
 import TestOption
 
--- MODEL
-type alias ABTest =
-    { left: TestOption.Model
-    , right: TestOption.Model
-    , description: String
-    }
 
-init : (ABTest, Cmd Msg)
+-- MODEL
+
+
+type alias ABTest =
+  { left : TestOption.Model
+  , right : TestOption.Model
+  , description : String
+  }
+
+
+init : ( ABTest, Cmd Msg )
 init =
-  (ABTest (fst TestOption.init) (fst TestOption.init) "Image comparison description"
-  , Cmd.none)
+  ( ABTest
+      (fst TestOption.init)
+      (fst TestOption.init)
+      "Image comparison description"
+  , Cmd.none
+  )
+
 
 
 -- UPDATE
+
+
 type Msg
   = Left TestOption.Msg
   | Right TestOption.Msg
   | Reset
 
-update : Msg -> ABTest -> (ABTest, Cmd Msg)
+
+update : Msg -> ABTest -> ( ABTest, Cmd Msg )
 update msg model =
   case msg of
     Left subMsg ->
@@ -33,24 +44,32 @@ update msg model =
         updatedTestOption =
           TestOption.update subMsg model.left
       in
-        ({ model | left = fst updatedTestOption }, Cmd.map Left (snd updatedTestOption))
+        ( { model | left = fst updatedTestOption }
+        , Cmd.map Left (snd updatedTestOption)
+        )
+
     Right subMsg ->
       let
         updatedTestOption =
           TestOption.update subMsg model.right
       in
-        ({ model | right = fst updatedTestOption }, Cmd.map Right (snd updatedTestOption))
+        ( { model | right = fst updatedTestOption }
+        , Cmd.map Right (snd updatedTestOption)
+        )
+
     Reset ->
       init
 
 
+
 -- VIEW
+
+
 view : ABTest -> Html Msg
 view model =
   div []
-    [
-      Html.App.map Left (TestOption.view model.left),
-      Html.App.map Right (TestOption.view model.right)
+    [ Html.App.map Left (TestOption.view model.left)
+    , Html.App.map Right (TestOption.view model.right)
     , div [] [ text model.description ]
     , button [ onClick Reset ] [ text "Reset" ]
     ]
--- TestOption.elm
+++ TestOption.elm
@@ -6,39 +6,51 @@ import Html.Events exposing (onClick)
 import Random
 import Debug
 
--- MODEL
-type alias Model =
-    { votes: Int
-    , image: String
-    }
 
-init : (Model, Cmd Msg)
+-- MODEL
+
+
+type alias Model =
+  { votes : Int
+  , image : String
+  }
+
+
+init : ( Model, Cmd Msg )
 init =
-  (Model 0 "Image name", Cmd.none)
+  ( Model 0 "Image name", Cmd.none )
+
+
 
 -- UPDATE
+
+
 type Msg
   = Choose
   | Randomize
   | NewVotes Int
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg testOption =
   case msg of
     Choose ->
       ( { testOption | votes = testOption.votes + 1 }, Cmd.none )
+
     Randomize ->
-      ( testOption, Random.generate NewVotes (Random.int 1 10))
+      ( testOption, Random.generate NewVotes (Random.int 1 10) )
+
     NewVotes newVotes ->
       ( { testOption | votes = newVotes }, Cmd.none )
 
+
+
 -- VIEW
+
 
 view : Model -> Html Msg
 view testOption =
   div []
-    [
-      div [ onClick Choose ] [ text (toString testOption) ]
+    [ div [ onClick Choose ] [ text (toString testOption) ]
     , div [ onClick Randomize ] [ text "Random" ]
     ]
-
